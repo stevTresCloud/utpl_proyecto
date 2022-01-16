28f87437b2db830a7cca0f372c6701f9a4460c94
stevTresCloud <steven.luna@trescloud.com>
2022-01-16 15:53:53 -0500

    [FIX][SL] Corrigiendo errores

--- proyecto_utpl/__manifest__.py
+++ proyecto_utpl/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Modulo Alex',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Sales',
     'description': '''
         Modulo Alex
