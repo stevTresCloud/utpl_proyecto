c68f5a8e63ea418999148dad7ac433015c59f7a7
stevTresCloud <steven.luna@trescloud.com>
2022-01-31 06:43:08 -0500

    [IMP][SL] Última actualización

---
+++ proyecto_utpl/migrations/14.0.2.7/post-migration.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Trescloud <https://www.trescloud.com>
+
+from openupgradelib import openupgrade
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.logging()
+def set_values(env):
+    sql = '''
+    SELECT id
+    FROM ir_translation 
+    WHERE value ilike 'Start Survey'
+    '''
+    env.cr.execute(sql)
+    records = env.cr.dictfetchall()
+    for record in records:
+        translation = env['ir.translation'].browse(record['id'])
+        translation.write({'value': 'Empezar Evaluación'})
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    set_values(env)
