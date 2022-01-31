4affe86cf8c03e5c298b34c18af01ac4dc21d667
stevTresCloud <steven.luna@trescloud.com>
2022-01-31 06:14:19 -0500

    [IMP][SL] Mejorando vista

--- proyecto_utpl/__manifest__.py
+++ proyecto_utpl/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Modulo Alex',
-    'version': '2.5',
+    'version': '2.6',
     'category': 'Sales',
     'description': '''
         Modulo Alex
